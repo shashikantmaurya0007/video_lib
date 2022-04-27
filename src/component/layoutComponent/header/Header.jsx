b941cf0b51e498406a10436ab2fca35a2728e718
shashikantmaurya0007 <80745766+shashikantmaurya0007@users.noreply.github.com>
2022-04-27 14:40:54 +0530

    feat:mobile view navbar

--- src/component/layoutComponent/header/Header.jsx
+++ src/component/layoutComponent/header/Header.jsx
@@ -1,14 +1,72 @@
 import React from "react";
 import { useMobileLayout } from "../../../customHooks/index";
-
+import styles from "./Header.module.css";
+import DeckTwoToneIcon from "@mui/icons-material/DeckTwoTone";
+import OndemandVideoTwoToneIcon from "@mui/icons-material/OndemandVideoTwoTone";
+import TravelExploreTwoToneIcon from "@mui/icons-material/TravelExploreTwoTone";
+import PersonOutlineTwoToneIcon from "@mui/icons-material/PersonOutlineTwoTone";
+import MenuTwoToneIcon from "@mui/icons-material/MenuTwoTone";
 const Header = () => {
   var a = "hello";
   const displayMobileLayout = useMobileLayout();
   console.log(displayMobileLayout, a);
-  if (displayMobileLayout) return <h1>mobile layout</h1>;
+  if (displayMobileLayout)
+    return (
+      <header>
+        <nav className={`${styles.nav_container}`}>
+          <div className={`${styles.nav_logo} headline`}>
+            <MenuTwoToneIcon className={`${styles.hamburger}`} />
+            Review
+          </div>
+        </nav>
+        <div className={`${styles.navbar_search_con}`}>
+          <input
+            className={`${styles.navbar_search}`}
+            type="text"
+            placeholder="search..."
+          />
+          <button className={`${styles.navbar_search_btn}`}>
+            <TravelExploreTwoToneIcon />
+          </button>
+        </div>
+      </header>
+    );
   return (
     <header>
-      <nav></nav>
+      <nav className={`${styles.nav_container}`}>
+        <div className={`${styles.nav_logo} headline`}>Review</div>
+
+        <div className={styles.btn_container}>
+          <button>
+            <DeckTwoToneIcon />
+
+            <p className={`text-btn`}> Home</p>
+          </button>
+          <button>
+            <OndemandVideoTwoToneIcon />
+
+            <p className={`text-btn`}> explore</p>
+          </button>
+        </div>
+
+        <div className={`${styles.navbar_search_con}`}>
+          <input
+            className={`${styles.navbar_search}`}
+            type="text"
+            placeholder="search..."
+          />
+          <button className={`${styles.navbar_search_btn}`}>
+            <TravelExploreTwoToneIcon />
+          </button>
+        </div>
+        <div className="">
+          <button>
+            <PersonOutlineTwoToneIcon />
+
+            <p className={`text-btn`}> Hi, user</p>
+          </button>
+        </div>
+      </nav>
     </header>
   );
 };
