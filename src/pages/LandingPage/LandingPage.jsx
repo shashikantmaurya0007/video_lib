6cc6de026bf4be9465e41e5724ac391a995f1929
shashikantmaurya0007 <80745766+shashikantmaurya0007@users.noreply.github.com>
2022-04-30 23:42:50 +0530

    feat: added carousel

---
+++ src/pages/LandingPage/LandingPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Carousel from "./Carousel";
+
+const LandingPage = () => {
+  return (
+    <div>
+      <Carousel />
+    </div>
+  );
+};
+
+export { LandingPage };
