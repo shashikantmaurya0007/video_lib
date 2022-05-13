1b3ea1a2cbf2a33a1af911d69f58495aa503d31d
shashikant maurya <shashikantmaurya0007@gmail.com>
2022-05-13 19:03:10 +0530

    feat:added navlink for redirection to explore page with respected category

--- src/pages/LandingPage/Category.jsx
+++ src/pages/LandingPage/Category.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 import { Transition } from "react-transition-group";
+import { useNavigate } from "react-router-dom";
 
 import { useCategory } from "../../services/categoryServices/index";
 import styles from "./LandingPage.module.css";
 import DoubleArrowTwoToneIcon from "@mui/icons-material/DoubleArrowTwoTone";
+import { filterAction } from "../../store/Explore/filtervideo-slice";
+import { useDispatch } from "react-redux";
 const Category = () => {
-  const { loading, error, categories } = useCategory();
+  const { categories } = useCategory();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const navigateToSelectedCategory = (selectedCategory) => {
+    dispatch(filterAction.setFilterCategory(selectedCategory));
+    navigate("/explore");
+  };
 
   return (
     <div>
@@ -13,32 +22,35 @@ const Category = () => {
         <h1>Courses </h1>
         <img src="/assets/brush_stroke.png" alt="" />
       </div>
-      {categories?.map(({ _id, categoryName, imgsrc, description }, index) => {
-        return (
-          <Transition in={true} mountOnEnter unmountOnExit>
-            {
-              // ${styles["category_left_"+state]}
-              (state) => (
-                <section className={` ${styles.category_card}`}>
-                  <section className={`${styles.category_desc}`}>
-                    <h1 className="category-heading">{categoryName}</h1>
-                    <p className="category-paragraph">{description}</p>
-                    <button
-                      className={`rounded_btn ${styles.category_redirect_btn}`}
-                    >
-                      <DoubleArrowTwoToneIcon />
-                    </button>
-                  </section>
+      {categories?.map(
+        ({ _id, categoryName, imgsrc, description, clickValue }, index) => {
+          return (
+            <Transition key={_id} in={true} mountOnEnter unmountOnExit>
+              {
+                // ${styles["category_left_"+state]}
+                (state) => (
+                  <section className={` ${styles.category_card}`}>
+                    <section className={`${styles.category_desc}`}>
+                      <h1 className="category-heading">{categoryName}</h1>
+                      <p className="category-paragraph">{description}</p>
+                      <button
+                        className={`rounded_btn ${styles.category_redirect_btn}`}
+                        onClick={() => navigateToSelectedCategory(clickValue)}
+                      >
+                        <DoubleArrowTwoToneIcon />
+                      </button>
+                    </section>
 
-                  <section className={`${styles.category_img_con}`}>
-                    <img src={imgsrc} className="img-rounded" alt="" />
+                    <section className={`${styles.category_img_con}`}>
+                      <img src={imgsrc} className="img-rounded" alt="" />
+                    </section>
                   </section>
-                </section>
-              )
-            }
-          </Transition>
-        );
-      })}
+                )
+              }
+            </Transition>
+          );
+        }
+      )}
     </div>
   );
 };
