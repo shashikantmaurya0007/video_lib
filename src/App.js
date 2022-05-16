981b721855303f8b7df3f5ae142c7b690a6b4884
shashikant maurya <shashikantmaurya0007@gmail.com>
2022-05-16 18:32:04 +0530

    feat:added intitial updation of redux on page reload

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import "./App.css";
 import MockmanEs from "mockman-js";
-import { useDispatch } from "react-redux";
 import Header from "./component/LayoutComponent/header/Header";
 import {
   SidebarLayoutPage,
@@ -13,16 +12,10 @@ import {
 import { RequireAuth } from "./component";
 import { Portal } from "./PortalComponent/Portal";
 import { Login, Signup } from "./component";
-import { fetchVideos } from "./store/Explore/videolisting-actions";
 
 import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
-import { useEffect } from "react";
 
 function App() {
-  const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(fetchVideos());
-  }, [dispatch]);
   return (
     <div className="App">
       <Router>
--- src/index.js
+++ src/index.js
@@ -5,10 +5,11 @@ import App from "./App";
 import { makeServer } from "./server";
 import { Provider } from "react-redux";
 import { store } from "./store/index";
+import { initialAppLoadingAction } from "./store/InitialAppLoadingAction/initialAppLoadingAction";
 // import { fetchVideos } from "./store/Explore/videolisting-actions";
 // Call make Server
 makeServer();
-
+store.dispatch(initialAppLoadingAction());
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
