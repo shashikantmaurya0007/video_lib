457431f7bc4aa8ce3b896d6586f1605455743c79
shashikant maurya <shashikantmaurya0007@gmail.com>
2022-05-20 11:13:53 +0530

    feat:explore page if list is empty

--- src/pages/LikePage/LikePage.js
+++ src/pages/LikePage/LikePage.js
@@ -1,15 +1,17 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { LikedVideos } from "../../component/Like/LikedVideos";
+import { NoVideoExplore } from "../../GeneralComponent/NoVideoExplore/NoVideoExplore";
 import { TitleVideoCount } from "../../GeneralComponent/TitleViedoCount/TitleVideoCount";
 
 const LikePage = () => {
   const likedvideo = useSelector((state) => state.like.likedvideo);
-  const isLikedListEmpty = likedvideo?.length;
+  const isLikedListEmpty = likedvideo?.length == 0;
   return (
     <>
       <TitleVideoCount title={"Liked Videos"} count={likedvideo.length} />
-      <LikedVideos />
+      {!isLikedListEmpty && <LikedVideos />}
+      {isLikedListEmpty && <NoVideoExplore />}
     </>
   );
 };
