e5f5f55df0082f6ee8c897308b661e7b865b2aac
shashikant maurya <shashikantmaurya0007@gmail.com>
2022-05-20 11:16:34 +0530

    removed:unused import

--- src/component/Like/LikeCustomHook/useLikeAndDislikeVideo.js
+++ src/component/Like/LikeCustomHook/useLikeAndDislikeVideo.js
@@ -1,5 +1,5 @@
 import { useDispatch, useSelector } from "react-redux";
-import { useLocation, useNavigate, Navigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { useDebounce } from "../../../customHooks/useDebounce";
 import { dislikeVideoAction } from "../../../store/Like/dislike-action";
 import { likeVideoAction } from "../../../store/Like/like-action";
